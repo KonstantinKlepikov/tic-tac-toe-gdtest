7b1bb73f6cbba80019bb73cee7c57e1d04532afb
Konstantin Klepikov <oformleno@gmail.com>
2024-07-14 00:29:15 +0300

    fin

--- gameplay/board.gd
+++ gameplay/board.gd
@@ -1,5 +1,14 @@
 extends Node2D
 var player: int = 1
+var screensize: Vector2
+var offset: Vector2
+
+
+func _ready():
+	screensize = get_viewport().get_visible_rect().size
+	position = screensize / 2
+	offset = position
+	$PlayerNum.text = str(player)
 
 
 func _input(event):
@@ -14,12 +23,12 @@ func create_symbol(next_player: int, new_position):
 		var create_x = preload("res://gameplay/x.tscn")
 		var get_x = create_x.instantiate()
 		add_child(get_x)
-		get_x.position = new_position
+		get_x.position = new_position - offset
 	else:
 		var create_o = preload("res://gameplay/o.tscn")
 		var get_o = create_o.instantiate()
 		add_child(get_o)
-		get_o.position = new_position
+		get_o.position = new_position - offset
 		
 		
 func update_player():
@@ -27,3 +36,4 @@ func update_player():
 		player = 2
 	else:
 		player = 1
+	$PlayerNum.text = str(player)
